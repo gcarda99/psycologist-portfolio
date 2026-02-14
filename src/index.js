b164ef941719c956ed45c703f8dbd60e6410fc9c
gcarda99 <g.cardaropoli99@gmail.com>
2026-02-14 15:30:21 +0100

    update react to version 18

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
-
-
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.render(
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>,
-  document.getElementById('root')
-);
 
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <ThemeContextProvider>
+        <App/>
+    </ThemeContextProvider>
+);
 
 reportWebVitals();
