6dbbdf56fb4571f8d2e397e69a6c6f74b327c735
Giuseppe Cardaropoli <57315337+gcarda99@users.noreply.github.com>
2026-03-04 22:11:30 +0100

    feat: use React.lazy for all page-level routes to avoid ad blocker blocking

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, {useContext} from 'react';
-import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import React, { useContext, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
-import {ThemeContext} from './contexts/ThemeContext';
-import {Main, PrivacyPolicy} from './pages'
-import {BackToTop} from './components'
-import ScrollToTop from './utils/ScrollToTop'
+import { ThemeContext } from './contexts/ThemeContext';
+import { Main } from './pages';
+import { BackToTop } from './components';
+import ScrollToTop from './utils/ScrollToTop';
 
-import './App.css'
+import './App.css';
+
+const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy/PrivacyPolicy'));
+// const BlogPage = lazy(() => import('./pages/Blog/BlogPage'));
+// const ProjectPage = lazy(() => import('./pages/Project/ProjectPage'));
 
 function App() {
-
-    const {theme} = useContext(ThemeContext);
-    console.log("used theme: " + theme.type);
+    const { theme } = useContext(ThemeContext);
+    console.log('used theme: ' + theme.type);
 
     return (
         <div className="app">
             <Router>
-                <ScrollToTop/>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                    {/*<Route path="/blog" element={<BlogPage />} />*/}
-                    {/*<Route path="/projects" element={<ProjectPage />} />*/}
-
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
+                <ScrollToTop />
+                <Suspense fallback={<div />}>
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                        {/* <Route path="/blog" element={<BlogPage />} /> */}
+                        {/* <Route path="/projects" element={<ProjectPage />} /> */}
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Routes>
+                </Suspense>
             </Router>
-            <BackToTop/>
+            <BackToTop />
         </div>
     );
 }
