2b623391b05460ee228c8eb68124cfc290507a80
gcarda99 <g.cardaropoli99@gmail.com>
2026-04-07 17:30:27 +0200

    change address

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,7 +2,6 @@ export const contactsData = {
     whatsapp: 'https://wa.me/393317047398',
     email: 'm.e.albano99@gmail.com',
     phone: '+39 3317047398',
-    address1: 'Mercato San Severino,',
-    address2: 'anche Online',
-    sheetAPI: ''
+    address1: 'Via Carmine Amato, 1/24, 84085 Piazza del Galdo-Sant\'angelo (SA)',
+    address2: 'anche Online'
 }
